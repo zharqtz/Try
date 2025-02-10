841b27d6780ddfc8b169feec432b4873c734d5dc
zharqtz <sombrax043@gmail.com>
2025-02-10 12:35:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Estás segura?",
+    "Muy muy segura???",
+    "Porfavorcito?",
+    "Amor porfavor...",
+    "Solo piensalo, te amo muto!",
+    "Voy a estar muy triste si sigues así...",
+    "...",
+    "amor...",
+    "Está bien, pararé...",
+    "Era broma, dale amorcito, porfi💞"
 ];
 
 let messageIndex = 0;
